fbf334aa4bebe42e1da4eadf7119c491ff9457f0
SaadbinGhaffar <saadghaffar97@gmail.com>
2025-11-01 22:27:36 +0500

    added Modal Loader Configuration

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,15 @@
+
+
+class   GraphBuilder():
+
+    def __ini__(self):
+        pass
+
+    def agent_function(self):
+        pass
+
+    def build_graph(self):
+        pass
+
+    def __call__(self):
+        pass
